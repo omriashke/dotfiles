a61b15604b839963b16b9b2e00ced0d0d830022b
Omri Askenazi <omri.ashke@gmail.com>
2026-03-10 23:24:11 +0200

    Update shader configuration in ghosty and toggle-ghostty-shader script to use specific shader files; enhance shader enabling/disabling logic.

--- toggle-ghostty-shader.sh
+++ toggle-ghostty-shader.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 SHADER_NAME="${1:-starfield-colors}"
-CONF_PATH="$HOME/Library/Application Support/com.mitchellh.ghostty/shaders.conf"
-SHADER="$HOME/Development/dotfiles/shaders/${SHADER_NAME}.glsl"
+CONFIG="$HOME/Development/dotfiles/ghosty"
+SHADER="/Users/omriaskenazi/Development/dotfiles/shaders/${SHADER_NAME}.glsl"
 
 if [ ! -f "$SHADER" ]; then
     echo "Shader not found: $SHADER" >&2
     exit 1
 fi
 
-if [ -e "$CONF_PATH" ]; then
-    rm "$CONF_PATH"
+if grep -q "^custom-shader" "$CONFIG"; then
+    sed -i '' 's/^custom-shader/# custom-shader/' "$CONFIG"
+    echo "Shaders disabled"
 else
-    cat > "$CONF_PATH" <<EOF
-custom-shader = $SHADER
-custom-shader-animation = true
-EOF
+    sed -i '' "s|^# custom-shader = .*|custom-shader = ${SHADER}|" "$CONFIG"
+    sed -i '' 's/^# custom-shader-animation/custom-shader-animation/' "$CONFIG"
+    echo "Shaders enabled: $SHADER_NAME"
 fi
 
 pkill -USR2 ghostty
